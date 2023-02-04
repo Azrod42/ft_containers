da0afd0a2a0974460a425dda4ba84c03c7b96034
azrod42 <tom.sorabella@gmail.com>
2023-02-04 18:37:04 +0100

    Autopush Makefile

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -1,24 +1,30 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-# include<iostream>
+# include <iostream>
+# include <stdexpected>
+# include <cmath>
+# include <cstdio>
 
-template < typename T, class Alloc = std::allocator<T> >
-class vector {
-	private:
-		T*				_vector;
-		size_t			_size;
-		allocator_type	_allocator;
+namespace ft
+{
+	template <typename T, class Alloc = std::allocator<T> >
+	class vector {
+		private:
+			T*				_vector;
+			size_t			_size;
+			allocator_type	_alloc;
 
-	public:
-		//constructor
-		explicit vector (const allocator_type& alloc = allocator_type());
-		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
-		template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-		vector (const vector& x);
-		//destructor
-		~vector();
-		//operator
-		vector		&operator=(const vector<T> &copy);
-};
+		public:
+			//constructor
+			explicit vector (const allocator_type& alloc = allocator_type());
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+			template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+			vector (const vector& x);
+			//destructor
+			~vector();
+			//operator
+			vector		&operator=(const vector<T> &copy);
+	};
+}
 #endif
