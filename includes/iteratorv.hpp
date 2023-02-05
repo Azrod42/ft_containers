ba389f11aaab0c242b91b6a42d533d0b900f36d1
azrod42 <tom.sorabella@gmail.com>
2023-02-05 18:11:22 +0100

    Autopush Makefile

--- includes/iteratorv.hpp
+++ includes/iteratorv.hpp
@@ -24,22 +24,22 @@ namespace ft
 				return (*this);
 			};
 			bool		operator==(const Iteratorv &rhs){
-				return (this->_ptr == rhs);
+				return (this->_ptr == rhs._ptr);
 			};
 			bool		operator!=(const Iteratorv &rhs){
-				return (this->_ptr == rhs);
+				return (this->_ptr == rhs._ptr);
 			};
 			pointer	operator*() {
 				return (_ptr);
-			}
+			};
 			reference	operator->() {
 				return (*_ptr);
-			}
-			Iteratorv	operator++(){
+			};
+			Iteratorv	&operator++(){
 				this->_ptr++;
 				return (*this);
 			};
-			Iteratorv	operator--(){
+			Iteratorv	&operator--(){
 				this->_ptr--;
 				return (*this);
 			};
@@ -64,27 +64,133 @@ namespace ft
 				return (temp);
 			};
 			bool		operator<(const Iteratorv &rhs){
-				return (this->_ptr < rhs);
+				return (this->_ptr < rhs._ptr);
 			};
 			bool		operator<=(const Iteratorv &rhs){
-				return (this->_ptr <= rhs);
+				return (this->_ptr <= rhs._ptr);
 			};
 			bool		operator>(const Iteratorv &rhs){
-				return (this->_ptr > rhs);
+				return (this->_ptr > rhs._ptr);
 			};
 			bool		operator>=(const Iteratorv &rhs){
-				return (this->_ptr >= rhs);
+				return (this->_ptr >= rhs._ptr);
 			};
-			Iteratorv	operator+=(size_t i){
+			Iteratorv	&operator+=(size_t i){
 				this->_ptr += i;
 				return (*this);
 			};
-			Iteratorv	operator-=(size_t i){
+			Iteratorv	&operator-=(size_t i){
 				this->_ptr -= i;
 				return (*this);
 			};
-			Iteratorv	operator[](size_t i){
-				return (this->_ptr[i]);
+			value_type	&operator[](size_t i){
+				return (*this->_ptr[i]);
+			};
+	};
+	template <class T> 
+	class Riteratorv : public Iteratorv<T>
+	{
+		public:
+			typedef T	value_type;
+			typedef T*	pointer;
+			typedef T&	reference;
+		public:
+			Riteratorv(){};
+			~Riteratorv(){};
+			Riteratorv(pointer ptr) : _ptr(ptr){};
+			Riteratorv(const Riteratorv &rhs){ *this = rhs; };
+			Riteratorv 	&operator=(const Riteratorv &rhs) {
+				this->_ptr = rhs._ptr;
+				return (*this);
+			};
+			bool		operator==(const Riteratorv &rhs){
+				return (this->_ptr == rhs._ptr);
+			};
+			bool		operator!=(const Riteratorv &rhs){
+				return (this->_ptr == rhs._ptr);
+			};
+			reference	operator*() {
+				return (*this->_ptr);
+			};
+			pointer		operator->() {
+				return (this->_ptr);
+			};
+			Riteratorv	&operator++(){
+				this->_ptr--;
+				return (*this);
+			};
+			Riteratorv	&operator--(){
+				this->_ptr++;
+				return (*this);
+			};
+			Riteratorv	operator++(int){
+				Riteratorv temp(*this);
+				this->_ptr--;
+				return (temp);
+			};
+			Riteratorv	operator--(int){
+				Riteratorv temp(*this);
+				this->_ptr++;
+				return (temp);
+			};
+			Riteratorv	operator+(size_t i) {
+				Riteratorv	temp(*this);
+				temp._ptr += i;
+				return (temp);
+			};
+			Riteratorv	operator-(size_t i) {
+				Riteratorv	temp(*this);
+				temp._ptr -= i;
+				return (temp);
+			};
+			bool		operator<(const Riteratorv &rhs){
+				return (this->_ptr < rhs);
+			};
+			bool		operator<=(const Riteratorv &rhs){
+				return (this->_ptr <= rhs);
+			};
+			bool		operator>(const Riteratorv &rhs){
+				return (this->_ptr > rhs);
+			};
+			bool		operator>=(const Riteratorv &rhs){
+				return (this->_ptr >= rhs);
+			};
+			Riteratorv	&operator+=(size_t i){
+				this->_ptr += i;
+				return (*this);
+			};
+			Riteratorv	&operator-=(size_t i){
+				this->_ptr -= i;
+				return (*this);
+			};
+			value_type	&operator[](size_t i){
+				return (*this->_ptr[i]);
+			};
+	};
+	template <class T> 
+	class Citeratorv : public Iteratorv<T>
+	{
+		public:
+			typedef T	value_type;
+			typedef T*	pointer;
+			typedef T&	reference;
+		public:
+			Citeratorv(){};
+			~Citeratorv(){};
+			Citeratorv(pointer ptr) : _ptr(ptr){};
+			Citeratorv(const Citeratorv &rhs){ *this = rhs; };
+			Citeratorv 	&operator=(const Citeratorv &rhs) {
+				this->_ptr = rhs._ptr;
+				return (*this);
+			};
+			const reference	operator*() {
+				return (*this->_ptr);
+			};
+			const pointer	operator->() {
+				return (this->_ptr);
+			};
+			const value_type	&operator[](size_t i){
+				return (*this->_ptr[i]);
 			};
 	};
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include"./includes/vector.hpp"
+#include"./includes/iteratorv.hpp"
+#include<vector>
 
 int main(void)
 {
-	ft::vector<int> test(5);
+	// ft::vector<int> test(5);
+	std::vector<int> tete(5);
+
+	tete.push_back(5);
+	tete.push_back(4);
+	tete.push_back(3);
+	tete.push_back(2);
+	tete.push_back(1);
+
+	typedef typename ft::Iteratorv<std::vector<int> > it;
+
+	it = tete.begin();
+
 
 	// test.~vector();
 }
