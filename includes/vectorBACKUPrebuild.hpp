be53a1355d6b82eb9936f370a3ceb9d908b402d5
azrod42 <tom.sorabella@gmail.com>
2023-02-12 14:52:56 +0100

    Autopush Makefile

--- includes/vectorBACKUPrebuild.hpp
+++ includes/vectorBACKUPrebuild.hpp
@@ -31,36 +31,117 @@ namespace ft
 			allocator_type	_alloc;
 
 		public:
-			///////////////////////////////////////
-			//			Iterators:               //
-			///////////////////////////////////////
-			explicit vector (const allocator_type& alloc = allocator_type());
-			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()); 
+			//constructor
+			explicit vector(const allocator_type& alloc = allocator_type())
+			: _vector(0), _size(0), _size_fill(0), _alloc(alloc){
+				_vector = _alloc.allocate(0);
+			};
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) 
+			: _vector(0), _size(n), _size_fill(0), _alloc(alloc){
+				_vector = _alloc.allocate(0);
+				insert(begin(), n, val);
+			};
 			template <class InputIterator>
-			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-			vector (const vector& x);
-			vector &operator=(const vector &copy);
-			~vector();
+			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+			: _vector(0), _size(0), _size_fill(0), _alloc(alloc){
+				this->_vector = this->_alloc.allocate(0);
+				assign(first, last);
+			};
+			vector (const vector& x){
+				*this = x;
+			};
+			vector &operator=(const vector &copy) {
+				if (this->_vector != 0)
+					this->_alloc.deallocate(this->_vector, this->_size);
+				this->_size = copy._size;
+				this->_size_fill = copy._size_fill;
+				this->_alloc = copy.get_allocator();
+				this->_vector = this->_alloc.allocate(0);
+				assign(copy.begin(), copy.end());
+				return (*this);
+			}
 			///////////////////////////////////////
 			//			Iterators:               //
 			///////////////////////////////////////
-			iterator				begin();
-			const_iterator			begin() const;
-			iterator 				end();
-			const_iterator			end() const;
-			reverse_iterator 		rbegin();
-			const_reverse_iterator	rbegin() const;
-			reverse_iterator		rend();
-			const_reverse_iterator	rend() const ;
+			iterator		begin(){
+				return (iterator(&this->_vector[0]));
+			};
+			const_iterator	begin() const{
+				return (const_iterator(&this->_vector[0]));
+			};
+			iterator end(){
+				return (iterator(&this->_vector[this->_size_fill]));
+			};
+			const_iterator end() const{
+				return (const_iterator(&this->_vector[this->_size_fill]));
+			};
+			reverse_iterator rbegin(){
+				return(reverse_iterator(&this->_vector[this->_size_fill]));
+			}
+			const_reverse_iterator rbegin() const{
+				return(const_reverse_iterator(&this->_vector[this->_size_fill]));
+			};
+			reverse_iterator rend(){
+				return(reverse_iterator(&this->_vector[0]));
+			};
+			const_reverse_iterator rend() const {
+				return(const_reverse_iterator(&this->_vector[0]));
+			};
 			///////////////////////////////////////
 			//			Capacity:               //
 			///////////////////////////////////////
-			size_type size() const;
-			size_type max_size() const;
-			void resize (size_type n, value_type val = value_type());
-			size_type capacity() const;
-			bool empty() const;
-			void reserve (size_type n);
+			size_type size() const{
+				return (this->_size_fill);
+			};
+			size_type max_size() const{
+				return (std::numeric_limits<size_type>::max() / sizeof(value_type));
+			};
+			void resize (size_type n, value_type val = value_type()){
+				pointer rep;
+				rep = this->_alloc.allocate(n);
+				for(size_type i = 0; i < this->_size_fill; i++){
+					rep[i] = this->_vector[i];
+				}
+				for(size_type i = this->_size_fill; i < this->_size; i++){
+					rep[i] = val;
+				}
+				this->_alloc.deallocate(this->_vector, this->_size);
+				this->_vector = rep;
+				this->_size = n;
+			};
+			size_type capacity() const{
+				return (this->_size);
+			};
+			bool empty() const {
+				return(this->_size_fill == 0);
+			};
+			void reserve (size_type n){
+				if (n > this->_size)
+				{
+				T	*dupli = new T[n - 1];
+				for (size_type i = 0; i < this->_size; ++i)
+				{
+					dupli[i] = this->_vector[i];
+					std::cout << dupli[i] << " " << this->_vector[i] << std::endl;
+				}
+				for (size_type i = 0; i < this->_size; ++i)
+					this->_alloc.destroy(this->_vector + i);
+				this->_alloc.deallocate(this->_vector, this->_size);
+				this->_vector = nullptr;
+				this->_size += 1;
+				this->_vector = this->_alloc.allocate(this->_size);
+				for(size_type i = 0; i < this->_size; ++i)
+					this->_alloc.construct(this->_vector + i, dupli[i]);
+				delete[] dupli;
+				}
+				// for(size_type i = 0; i <= this->_size_fill; i++){
+				// 	rep[i] = this->_vector[i];
+				// }
+				// std::cout << "deal" << std::endl;
+				// this->_alloc.deallocate(this->_vector, this->_size);
+				// this->_size = n;
+				// this->_vector = rep;
+			};
 			///////////////////////////////////////
 			//			Element access:          //
 			///////////////////////////////////////
--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@
 int main(void)
 {
 	{
-		ft::vector<int> tete(32);
+		ft::vector<int> tete(4);
 
+		std::cout << "here" << std::endl;
 		tete.push_back(299);
 		// tete.push_back(298);
 		// tete.push_back(297);
 		// tete.push_back(296);
 		// tete.push_back(295);
 
+		// std::cout << "---" << tete[33] << std::endl;
 		for (size_t i = 0; i < tete.size(); i++)
 			std::cout << tete[i] << std::endl;
 		std::cout << "=========" << std::endl;
@@ -22,13 +24,13 @@ int main(void)
 		std::cout << "=========" << std::endl;
 	}
 	{
-		std::vector<int> tete(32);
+		std::vector<int> tete(4);
 
 		tete.push_back(299);
-		tete.push_back(298);
-		tete.push_back(297);
-		tete.push_back(296);
-		tete.push_back(295);
+		// tete.push_back(298);
+		// tete.push_back(297);
+		// tete.push_back(296);
+		// tete.push_back(295);
 
 		for (size_t i = 0; i < tete.size(); i++)
 			std::cout << tete[i] << std::endl;
