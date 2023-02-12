82b9e4d3f17e7c0284b1cb50c372113076bdee13
azrod42 <tom.sorabella@gmail.com>
2023-02-12 10:19:05 +0100

    Autopush Makefile

--- includes/Iterator_traits.hpp
+++ includes/Iterator_traits.hpp
@@ -1,17 +1,17 @@
 #ifndef ITERATOR_TRAITS_HPP
 # define ITERATOR_TRAITS_HPP
 
-#include"iteator.hpp"
+#include"ft_include.hpp"
 
 namespace	ft {
-	template <classe Iterator>
+	template <class Iterator>
 	class iterator_traits {
 		typedef typename Iterator::difference_type		difference_type;
 		typedef typename Iterator::value_type			value_type;
 		typedef typename Iterator::pointer				pointer;
 		typedef typename Iterator::reference			reference;
 		typedef typename Iterator::iterator_category	iterator_category;
-	}
+	};
 
 	template <class T>
 	class iterator_traits<T*> {
@@ -20,7 +20,7 @@ namespace	ft {
 		typedef T*										pointer;
 		typedef T&										reference;
 		typedef std::random_access_iterator_tag			iterator_category;
-	}
+	};
 	template <class T>
 	class iterator_traits<const T*> {
 		typedef std::ptrdiff_t							difference_type;
@@ -28,7 +28,7 @@ namespace	ft {
 		typedef const T*								pointer;
 		typedef const T&								reference;
 		typedef std::random_access_iterator_tag			iterator_category;
-	}
+	};
 }
 
 #endif
---
+++ includes/ft_include.hpp
@@ -0,0 +1,8 @@
+#ifndef FT_INCLUDE_HPP
+# define FT_INCLUDE_HPP
+
+# include<iostream>
+# include"iteator.hpp"
+# include"Iterator_traits.hpp"
+
+#endif
