b99a818abbd64f9ce9804f0fc5f2db388c28e52f
azrod42 <tom.sorabella@gmail.com>
2023-02-12 14:58:39 +0100

    Autopush Makefile

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -55,150 +55,45 @@ namespace ft
 			///////////////////////////////////////
 			//			Capacity:               //
 			///////////////////////////////////////
-			size_type size() const;
-			size_type max_size() const;
-			void resize (size_type n, value_type val = value_type());
-			size_type capacity() const;
-			bool empty() const;
-			void reserve (size_type n);
+			size_type	size() const;
+			size_type	max_size() const;
+			void		resize (size_type n, value_type val = value_type());
+			size_type	capacity() const;
+			bool 		empty() const;
+			void 		reserve (size_type n);
 			///////////////////////////////////////
 			//			Element access:          //
 			///////////////////////////////////////
-			reference operator[] (size_type n){
-				return(this->_vector[n]);
-			};
-			const_reference operator[] (size_type n) const{
-				return (*this->_vector[n]);
-			};
-			reference at (size_type n) {
-				return (this->_vector[n]);
-			};
-			const_reference at (size_type n) const {
-				return (*this->_vector[n]);
-			};
-			reference front(){
-				return (this->_vector[0]);
-			};
-			const_reference front() const {
-				return (*this->_vector[0]);
-			};
-			reference back(){
-				return (this->_vector[this->_size_fill - 1]);
-			};
-			const_reference back() const {
-				return (*this->_vector[this->_size_fill - 1]);
-			};
-			value_type* data() {
-				return (this->_vector);
-			};
-			const value_type* data() const {
-				return (this->_vector);
-			};
+			reference 			operator[] (size_type n);
+			const_reference 	operator[] (size_type n) const;
+			reference 			at (size_type n);
+			const_reference 	at (size_type n) const;
+			reference 			front();
+			const_reference 	front() const;
+			reference			back();
+			const_reference 	back() const;
+			value_type*			data();
+			const value_type*	data() const;
 			///////////////////////////////////////
 			//			  Modifiers:             //
 			///////////////////////////////////////
 			template <class InputIterator>
-			void assign (InputIterator first, InputIterator last){
-				clear();
-				insert(begin(), first, last);	
-			};
-			void assign (size_type n, const value_type& val){
-				clear();
-				insert(begin(), n, val);
-			};
-			void push_back (const value_type& val){
-				if (this->_size_fill + 1 > this->_size)
-				{
-					std::cout << "reserve, size = "<< this->_size << " fill : " << this->_size_fill << std::endl;
-					reserve((this->_size + 1));
-					std::cout << "out reserve" << std::endl;
-				}
-				this->_vector[this->_size_fill] = val;
-				this->_size_fill += 1;
-			};
-			void pop_back(){
-				if (this->_size_fill)
-					this->_size_fill--;
-			};
-			iterator insert(iterator position, const value_type& val){
-					size_type i = 0;
-					iterator	it = begin();
-
-					if (this->_size < this->_size_fill + 1)
-						reserve(this->_size_fill + 1);
-					while (it + i != position)
-						i++;
-					if (this->_size_fill){
-					for (size_type j = this->_size_fill - 1; j > i; j--)
-					{
-						this->_vector[j] = this->_vector[j - 1];
-					}
-					}
-					this->_vector[i] = val;
-					this->_size_fill++;
-					return(iterator(&this->_vector[i]));
-					// while  (begin() != position)
-					// 	i++;
-					// pointer	ret = &this->_vector[i];
-					// if (this->_size < this->_size_fill + 1)
-					// 	reserve(this->_size_fill + 1);
-					// if (i != this->_size) 
-					// 	std::copy(position, end(), ret);
-					// this->_alloc.construct(this->_vector[i], val);
-					// this->_size_fill++;
-					// return (iterator(&this->_vector[i]));
-			};
-			void insert (iterator position, size_type n, const value_type& val){
-				for(size_type i = 0; i <= n; i++){
-					insert(position, val);
-				}
-			};
-
+			void 		assign (InputIterator first, InputIterator last);
+			void		assign (size_type n, const value_type& val);
+			void		push_back (const value_type& val);
+			void		pop_back();
+			iterator	insert(iterator position, const value_type& val);
+			void		insert (iterator position, size_type n, const value_type& val);
 			template <class InputIterator>
-			void insert (iterator position, InputIterator first, InputIterator last){
-				while (first != last){
-					position = (insert(position, *first)) + 1;
-					++first;
-				} 
-			};
-			iterator erase (iterator position){
-				iterator	it = position;
-
-				while ((it + 1) != end())
-					{
-						*it = *(it + 1);
-						it++;
-					}
-					*(it + 1) = 0;
-				this->_size_fill -= 1;
-				return (iterator(position));
-			};
-			iterator erase (iterator first, iterator last){
-				size_type i = 0;
-
-				while (first + i != last)
-				 	i++;
-				while (i > 0) {
-					erase(first);
-					i--;
-				}
-				return (iterator(first));
-			};
-			void swap (vector& x);
-			void clear(){
-				if (this->_size){
-					this->_alloc.deallocate(this->_vector, this->_size);
-					this->_vector = this->_alloc.allocate(0);
-					this->_size = 0;
-				} 
-			};
+			void		insert (iterator position, InputIterator first, InputIterator last);
+			iterator	erase (iterator position);
+			iterator	erase (iterator first, iterator last);
+			void		swap (vector& x);
+			void		clear();
 			///////////////////////////////////////
 			//			  Allocator:             //
 			///////////////////////////////////////
-			allocator_type get_allocator() const{
-				allocator_type ret = this->_alloc;
-				return (ret);
-			};
+			allocator_type get_allocator() const;
 	};
 }
 #endif
