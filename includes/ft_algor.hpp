07c2a2baf030312f7f0ade9d0ed2674d3df89cec
azrod42 <tom.sorabella@gmail.com>
2023-02-16 17:40:32 +0100

    Autopush Makefile

--- includes/ft_algor.hpp
+++ includes/ft_algor.hpp
@@ -66,8 +66,22 @@ namespace ft
 		typedef std:integral_constant<T,v>	type;
 		constexpr operator T() { return value; }
 	};
-	template <class T> struct is_integral : public ft::is_integral<T, false> {};
-	template <> struct is_integral<bool> : public ft::is_integral<bool, true> {};
+	template <class T> struct is_integral : public ft::integral_constant<T, false> {};
+	template <> struct is_integral<bool> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<char> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<char16_t> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<char32_t> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<wchar_t> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<signed char> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<short int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<long int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<long long int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned char> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned short int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned long int> : public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned long long int> : public ft::integral_constant<bool, true> {};
 	typedef integral_constant<bool,true> true_type;
 
 };
